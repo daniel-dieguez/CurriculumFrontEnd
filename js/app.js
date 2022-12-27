fb92995fe70baee507fd77511a1ee102b5c41631
Esteban <cddanguez2001@gmail.com>
2022-12-27 17:49:36 -0600

    mejora en formulario

--- js/app.js
+++ js/app.js
@@ -5,38 +5,54 @@
 var btnEnviar = document.getElementById('btnform');
 
 
-btnEnviar.addEventListener('click', () =>{
-  event.preventDefault()
+  const apodo = {
+    correos :  /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+  }
+
   
+
+
+
+btnEnviar.addEventListener('click', (e) =>{
+  event.preventDefault()
   
   var nombre = document.getElementById('name');
   var email = document.getElementById ('email');
-
- 
 
   const nombres = nombre.value.length;
   const emails = email.value.length;
   const todo = nombres + emails;
   
-  if(todo <= 5){
-    console.log("Error");    
+  if(todo === 0)
+   
+  {
+    setTimeout(() => {
+      console.log("Error");    
     Swal.fire({
       icon: 'error',
       title: 'Nombre o Correo mal escrito',
       text: 'Vuelva a revisar',
-    })
+    })    
+    }, 1000);
+     
 
 } else {
-  console.log(nombre.value + email.value);
+  setTimeout(() => {
+    console.log(nombre.value + email.value);
   Swal.fire({
     icon: 'success',
     title: 'Mensaje enviado ',
-    text: `Pronto el caballero se contactara con usted ${email.value }`,
-  })
+    text: `Pronto el caballero se contactara con usted`,
+  })    
+  }, 1000);
+  
 }
 
 form.reset();
 });
+
+
+//totito Juego 
 
 var btnreiniciar = document.getElementById("reiniciar");
 let comando = 'x';
@@ -57,6 +73,8 @@ btnreiniciar.addEventListener('click', function(){
     element.innerHTML= '';
   });
 })
+
+
 
 
 
